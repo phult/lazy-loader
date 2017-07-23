5b135615052222059365e5186cfd52917304b1d3
PhuLuong <phult.contact@gmail.com>
2017-07-24 00:16:35 +0700

    add error page

--- website/app/routes.php
+++ website/app/routes.php
@@ -12,6 +12,9 @@
 */
 
 Route::get("/", ["as" => "post:feed", "uses" => "PostController@feed"]);
-Route::get("/{postSlug}-c{postId}", ["as" => "post:view", "uses" => "PostController@view"])->where('postSlug', '[0-9a-zA-Z/_\-]+')->where('postId', '[0-9]+');
-Route::get("/trends", ["as" => "home:index", "uses" => "HomeController@index"]);
+Route::get("/latest", ["as" => "post:latest", "uses" => "PostController@latest"]);
 Route::get("/history", ["as" => "post:history", "uses" => "PostController@history"]);
+Route::get("/{postSlug}-c{postId}", ["as" => "post:view", "uses" => "PostController@view"])->where('postSlug', '[0-9a-zA-Z/_\-]+')->where('postId', '[0-9]+');
+Route::get("post/load-more", ["as" => "post:load-mode", "uses" => "PostController@loadMore"]);
+Route::get("/template", ["as" => "home:template", "uses" => "HomeController@template"]);
+Route::any("/error", "BaseController@error");
