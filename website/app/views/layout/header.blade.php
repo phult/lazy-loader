701de848bc2a1e626cb7a484770efbdfded45abd
PhuLuong <phult.contact@gmail.com>
2017-08-20 16:32:53 +0700

    optimize nav bar

--- website/app/views/layout/header.blade.php
+++ website/app/views/layout/header.blade.php
@@ -1,34 +1,49 @@
-<nav id="mainNav" class="navbar navbar-default navbar-fixed-top affix-top">
+<style type="">
+
+</style>
+<nav id="mainNav" class="navbar navbar-inverse navbar-fixed-top affix-top">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span> <i class="fa fa-bars"></i>
-                Menu
+            <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" rel="home" href="/" title="Home page">
-                <img style="max-width:40px; margin-top: -7px;" src="images/logo_original.png">
+            <!--a class="navbar-brand" rel="home" href="/" title="Home page">
+                <img style="max-width:35px; margin-top: -6px;" src="images/logo_original.png">
+            </a-->
+            <a  class="navbar-brand"
+                href="/"
+                style="text-align: center; float: none; line-height: 50px">
+                <img style="max-width:35px; margin-top: -6px;display: inherit" src="images/logo_original.png">
             </a>
+            <a class="navbar-brand pull-right" title="Search">
+                <i class="glyphicon glyphicon-search"></i>
+            </a>
+            <!--a class="navbar-brand pull-right" href="/history" title="History">
+                <i class="glyphicon glyphicon-time"></i>
+            </a-->
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <li class="">
-                    <a class="page-scroll" href="/latest">Latest</a>
+                    <a class="page-scroll" href="/latest"><i class="glyphicon glyphicon-globe"></i> Latest</a>
                 </li>
                 <li>
-                    <a class="page-scroll" href="/history">History</a>
+                    <a class="page-scroll" href="/history"><i class="glyphicon glyphicon-time"></i> History view</a>
                 </li>
                 <li>
-                    <a class="page-scroll" href="/about">How it works?</a>
+                    <a class="page-scroll" href="/about"><i class="glyphicon glyphicon-baby-formula"></i> How it works?</a>
                 </li>
             </ul>
-            <form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="">
+            <!--form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="">
                 <div class="form-group">
                     <input id="s" name="s" type="text" class="search-query form-control" autocomplete="off" placeholder="Search...">
                 </div>
-            </form>
+            </form-->
         </div>
         <!-- /.navbar-collapse -->
     </div>
