e131e46e5c52141d009438efb567b2a1f79b01e9
PhuLuong <phult.contact@gmail.com>
2017-08-26 20:32:17 +0700

    add search bar

--- website/app/views/layout/header.blade.php
+++ website/app/views/layout/header.blade.php
@@ -1,11 +1,18 @@
-<style type="">
-
-</style>
+<script>
+    $(document).on("click", "#search-button, #search-bar button[type='reset']", function(event) {
+        event.preventDefault();
+        if ($("#search-button").attr("aria-expanded") == "true") {
+            $("#search-input").focus();
+        } else {
+            $("#search-input").blur();
+        }
+    });
+</script>
 <nav id="mainNav" class="navbar navbar-inverse navbar-fixed-top affix-top">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#main-menu">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -18,16 +25,12 @@
                 style="text-align: center; float: none; line-height: 49px">
                 <img style="max-width:35px; margin-top: -2px;display: inherit" src="images/logo_original.png">
             </a>
-            <button id="search-button" type="button" class="navbar-toggle pull-right" title="Search">
+            <button id="search-button" type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#search-bar">
                 <i class="glyphicon glyphicon-search"></i>
             </button>
-            <!--a class="navbar-brand pull-right" href="/history" title="History">
-                <i class="glyphicon glyphicon-time"></i>
-            </a-->
         </div>
-
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="main-menu">
             <ul class="nav navbar-nav navbar-right">
                 <li class="">
                     <a class="page-scroll" href="/latest"><i class="glyphicon glyphicon-globe"></i>&nbsp&nbsp Latest posts</a>
@@ -45,7 +48,24 @@
                 </div>
             </form-->
         </div>
-        <!-- /.navbar-collapse -->
+        <div class="collapse navbar-collapse" id="search-bar">
+            <form class="navbar-form" role="search">
+    			<div class="input-group">
+    				<input id="search-input" name="s" type="text" class="form-control" placeholder="Search ...">
+    				<span class="input-group-btn">
+    					<button type="submit" class="btn btn-default">
+    						<span class="glyphicon glyphicon-search">
+    							<span class="sr-only">Search</span>
+    						</span>
+    					</button>
+                        <button type="reset" class="btn btn-default" data-toggle="collapse" data-target="#search-bar">
+            				<span class="glyphicon glyphicon-remove">
+            					<span class="sr-only">Close</span>
+            				</span>
+            			</button>
+    				</span>
+    			</div>
+    		</form>
+        </div>
     </div>
-    <!-- /.container-fluid -->
 </nav>
